90d3317765ecf92631eff4365afabd95c145d8cd
Erick de Souza <130837815+erickpereir0@users.noreply.github.com>
2024-05-18 03:03:18 -0300

    Update script.js

--- prova1/src/main/webapp/script.js
+++ prova1/src/main/webapp/script.js
@@ -1,31 +1,37 @@
-// ==============================================================
-// 		EVENTOS
 
-// RESET
-reset = function() {
-	// Aqui você cria uma requisição AJAX POST a ControllerServlet
-	// Você repassa, com a chave 'op' o parâmetro 'RESET'
-	// Se a requisição for bem sucedida, você executa:
-	// atualizaSessao() e window.location.href = "/prova1".
-	// Se não for bem sucedida, decida o que fazer.
+reset = function(id) {
+	let req = new XMLHttpRequest();
+	req.open("POST", "ControllerServlet", true);
+	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	req.onreadystatechange = () => {
+		if (req.readyState == 4) {
+			if (req.status == 200) {  
+			   atualizaSessao();
+			   window.location.href = "/prova1";
+			} else {
+				console.error("Erro na requisição: ", req.status);
+					}
+			}
+	}
+	req.send("op=RESET");
 }
 
-// NOVA AULA
+
 novaAula = function() {
 	window.location.href = "nova";
 }
 
-// CANCELA NOVA AULA (OU EDIÇÃO)
+
 calcelarNovaAula = function() {
 	window.location.href = "/prova1";
 }
 
-// EDITA UMA AULA COM ID ESPECIFICADO
+
 editarAula = function(id) {
 	window.location.href = "edit?id=" + id;
 }
 
-// ENVIA CONTEÚDO DA NOVA AULA
+
 enviarNovaAula = function() {
 	// obtém os valores a partir do formulário
 	let data = document.getElementById('data-id').value;
@@ -33,57 +39,77 @@ enviarNovaAula = function() {
 	let duracao = document.getElementById('dur-id').value;
 	let codDisciplina = document.getElementById('disc-id').value;
 	let assunto = document.getElementById('ass-id').value;
-	// verificando a validação
+
 	if (!validaNovaAula(data, horario, duracao, codDisciplina, assunto)) {
         document.getElementById('msg-id').style.display = 'block';
         return;
     }
-    // Aqui, você faz uma requisição AJAX POST a ControllerServlet e
-    // envia a chave 'op' valendo 'CREATE'. Envie, do mesmo modo, os parâmetros
-    // data, horario, duracao, codDisciplina e assunto.
-    // Se a requisição for bem sucedida, execute atualizaSessao() e
-    // window.location.href = "/prova1"
-    // Se não for bem sucedida, decida o que fazer
-}
-
-// ENVIA CONTEÚDO EM EDIÇÃO
-enviarEdit = function() {
-	// obtém os valores a partir do formulário
-	let id = document.getElementById('id').innerHTML;
-	let data = document.getElementById('data-id').value;
-	let horario = document.getElementById('hora-id').value;
-	let duracao = document.getElementById('dur-id').value;
-	let codDisciplina = document.getElementById('disc-id').value;
-	let assunto = document.getElementById('ass-id').value;
-	// Aqui, você faz uma requisição AJAX POST a ControllerServlet e
-    // envia a chave 'op' valendo 'UPDATE'. Envie, do mesmo modo, os parâmetros
-    // id, data, horario, duracao, codDisciplina e assunto.
-    // Se a requisição for bem sucedida, execute atualizaSessao() e
-    // window.location.href = "/prova1"
-    // Se não for bem sucedida, decida o que fazer
-}
-
-// DELETA UMA AULA
-deleta = function(id) {
-	// Aqui, você faz uma requisição AJAX POST a ControllerServlet e
-    // envia a chave 'op' valendo 'DELETE'. Envie, do mesmo modo, o parâmetro id
-    // Se a requisição for bem sucedida, execute atualizaSessao() e
-    // window.location.href = "/prova1"
-    // Se não for bem sucedida, decida o que fazer
 	let req = new XMLHttpRequest();
 	req.open("POST", "ControllerServlet", true);
 	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	req.onreadystatechange = () => {
-		if (req.readyState == 4 && req.status == 200) {
-			atualizaSessao();
-			window.location.href = "/prova1";
-		} else {
-			// O QUE FAZER SE DEU ERRADO
+	req.onreadystatechange = function() {
+		if (req.readyState == 4) {
+			if (req.status == 200) {  
+			   atualizaSessao();
+			   window.location.href = "/prova1";
+			} else {
+				console.error("Erro na requisição: ", req.status);
+					}
+			}
 		}
-	}
-	req.send("op=DELETE&id=" + id);
+	let parametros = "op=CREATE&data=" + encodeURIComponent(data) + "&horario=" + encodeURIComponent(horario) + "&duracao=" + encodeURIComponent(duracao) + "&codDisciplina=" + encodeURIComponent(codDisciplina) + "&assunto=" + encodeURIComponent(assunto);
+   	req.send(parametros);
+}
+enviarEdit = function() {
+    // obtém os valores a partir do formulário
+    let id = document.getElementById('register-id').value;
+    let data = document.getElementById('data-id').value;
+    let horario = document.getElementById('hora-id').value;
+    let duracao = document.getElementById('dur-id').value;
+    let codDisciplina = document.getElementById('disc-id').value;
+    let assunto = document.getElementById('ass-id').value;
+
+    let req = new XMLHttpRequest();
+
+
+    req.open("POST", "ControllerServlet", true);
+    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+
+    req.onreadystatechange = function() {
+        if (req.readyState == 4) {
+            if (req.status == 200) {
+                
+                atualizaSessao();
+                window.location.href = "/prova1";
+            } else {
+
+                console.error("Ocorreu um erro na requisição: " + req.status);
+            }
+        }
+    };
+
+    let params = "op=UPDATE&id=" + id + "&data=" + data + "&horario=" + horario + "&duracao=" + duracao + "&codDisciplina=" + codDisciplina + "&assunto=" + assunto;
+
+    req.send(params);
 }
 
+
+deleta = function(id) {
+    let req = new XMLHttpRequest();
+    req.open("POST", "ControllerServlet", true);
+    req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+    req.onreadystatechange = function() {
+        if (req.readyState == 4 && req.status == 200) {
+            atualizaSessao();
+            window.location.href = "/prova1";
+        } else{
+			
+		}
+    };
+    req.send("op=DELETE&id=" + id);
+}
 
 
 
@@ -93,9 +119,9 @@ const atualizaSessao = function() {
 	req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	req.onreadystatechange = () => {
 		if (req.readyState == 4 && req.status == 200) {
-			// O QUE FAZER SE DEU CERTO
+			console.log("A sessão foi atualizada com sucesso.");
 		} else {
-			// O QUE FAZER SE DEU ERRADO
+			console.log("Ocorreu um erro ao tentar atualizar a sessão.");
 		}
 	}
 	req.send("op=START_SESSION");
@@ -103,20 +129,30 @@ const atualizaSessao = function() {
 
 
 
-// ============================================================
-// 			VALIDAÇÕES
-
 validaNovaAula = function(data, horario, duracao, codDisciplina, assunto) {
-    // Examine os valores dos parâmetros deste método e decida se estão
-    // ou não validados. Este 'return true' provavelmente será alterado, não?
+    if (!data || !horario || !duracao || !codDisciplina || !assunto) {
+	document.getElementById('msg-vazio').style.display = 'block';
+        return false;
+    }
+
+    let dataRegex = /^\d{4}-\d{2}-\d{2}$/;
+    if (!dataRegex.test(data)) {
+	document.getElementById('msg-data').style.display = 'block';
+        return false;
+    }
+
+    let horarioRegex = /^\d{2}:\d{2}$/;
+    if (!horarioRegex.test(horario)) {
+	document.getElementById('msg-horario').style.display = 'block';
+        return false;
+    }
+
+    if (isNaN(parseFloat(duracao)) || parseFloat(duracao) <= 0) {
+	document.getElementById('msg-duracao').style.display = 'block';
+        return false; 
+    }
+
     return true;
 }
-
-
-
-
-
-// ===================================================================================
-// 		INICIALIZA O PROCESSAMENTO
 
 atualizaSessao();
